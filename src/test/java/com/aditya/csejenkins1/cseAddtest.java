6bd1a926dbfc6be93a51f6dfa29df5b72f21339c
Y.Siva Ganesh <ganeshyandra8@gmail.com>
2026-02-23 21:45:29 +0530

    first1 commit

--- src/test/java/com/aditya/csejenkins1/cseAddtest.java
+++ src/test/java/com/aditya/csejenkins1/cseAddtest.java
@@ -1,17 +1,21 @@
 package com.aditya.csejenkins1;
 
-public class cseAddtest {
-	class cseaddtest{
-    cseAdd c=new cseAdd();
-	     @Test
-		void csetest() {
-			int result=c.add(2,3);
-			assertEqual(5,result);
-		}
-		@Test
-		void csetest1() {
-			int result=c.mul(2,3);
-			assertEqual(6,result);
-	}
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+public class cseAddtest {
+
+    cseAdd c = new cseAdd();
+
+    @Test
+    void addTest() {
+        int result = c.add(2, 3);
+        assertEquals(5, result);
+    }
+
+    @Test
+    void mulTest() {
+        int result = c.mul(2, 3);
+        assertEquals(6, result);
+    }
 }
