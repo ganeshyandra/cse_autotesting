18105da4567e1496c303b9c0e564eb180a1c3ecb
Y.Siva Ganesh <ganeshyandra8@gmail.com>
2026-02-23 14:31:12 +0530

    first commit

--- src/main/java/com/aditya/csejenkins1/cseAdd.java
+++ src/main/java/com/aditya/csejenkins1/cseAdd.java
@@ -2,4 +2,10 @@ package com.aditya.csejenkins1;
 
 public class cseAdd {
 
+	public int add(int a, int b) {
+		return a+b;
+	}
+	public int mul(int a,int b) {
+	return a*b;
+}
 }
--- src/test/java/com/aditya/csejenkins1/cseAddtest.java
+++ src/test/java/com/aditya/csejenkins1/cseAddtest.java
@@ -1,5 +1,17 @@
 package com.aditya.csejenkins1;
 
 public class cseAddtest {
+	class cseaddtest{
+    cseAdd c=new cseAdd();
+	     @Test
+		void csetest() {
+			int result=c.add(2,3);
+			assertEqual(5,result);
+		}
+		@Test
+		void csetest1() {
+			int result=c.mul(2,3);
+			assertEqual(6,result);
+	}
 
 }
